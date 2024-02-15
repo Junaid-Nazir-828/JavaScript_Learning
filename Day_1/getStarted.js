d1a65edea8afbb25ef040885b330623c0cc45f9b
juniad-hub <junaidnazir7501@gmail.com>
2024-02-15 15:33:21 +0500

    First Day

---
+++ Day_1/getStarted.js
@@ -0,0 +1,23 @@
+"use strict";
+// let message;
+
+// message = 'Hello';
+// console.log(message)
+// // alert(message)
+
+// let user = 'Juniad',
+//     num = 2,
+//     user2 = 'Umair';
+// let $ = 1
+// let _ = 2
+
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+// ...when we need to pick a color
+let color = COLOR_ORANGE;
+color = 'false'
+alert(color); // #FF7F00
